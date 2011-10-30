f503c69bd7fac7dad28575f81391d80866014401
Adam <regality@gmail.com>
2011-10-29 18:11:13 -0600

    fixed session hijack bug

--- session/Session.php
+++ session/Session.php
@@ -31,7 +31,7 @@ abstract class Session {
       } else if (($this->config['session']['ip_sticky'] &&
                   $_SESSION['REMOTE_ADDR'] != $_SERVER['REMOTE_ADDR']) ||
                   $_SESSION['HTTP_USER_AGENT'] != $_SERVER['HTTP_USER_AGENT']) {
-         $this->clear();
+         $this->destroy();
       }
    }
 
